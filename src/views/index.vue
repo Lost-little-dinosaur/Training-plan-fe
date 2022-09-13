6adff53f88b783f238d46b1a3b8d178577683c81
Lost-little-dinosaur <3144794112@qq.com>
2022-09-13 19:22:02 +0800

    add addLesson.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,10 +2,11 @@
     <div class="common-layout">
         <el-container>
             <el-header>
-                <Aside></Aside>
+                <Aside @updateIndex="updateIndex"/>
             </el-header>
             <el-main>
-                <drag />
+                <drag v-if="nowIndex===1"/>
+                <addLesson v-if="nowIndex===2"/>
             </el-main>
             <el-footer>Footer</el-footer>
         </el-container>
@@ -13,14 +14,29 @@
 </template>
 
 <script lang="ts">
-    import Aside from "@/components/aside.vue";
-    import Drag from "@/components/drags.vue";
-    export default{
-        name: 'index',
-        components:{
-            Aside,
-            Drag
+import Aside from "@/components/aside.vue";
+import Drag from "@/components/drags.vue";
+import addLesson from "@/components/addLesson.vue"
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name: 'index',
+    methods: {
+        updateIndex(n: number) {
+            // alert("hahahaha");
+            this.nowIndex = n;
+        },
+    },
+    data() {
+        return {
+            nowIndex: 2
         }
-    } 
+    },
+    components: {
+        Aside,
+        Drag,
+        addLesson
+    }
+})
 
 </script>
