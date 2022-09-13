dde861d0b9c3c3c2d5032da09b6628299b6d7da7
lianenli <boyhoodgone@gmail.com>
2022-09-13 16:41:40 +0800

    first commit

--- src/components/drags.vue
+++ src/components/drags.vue
@@ -1,7 +1,40 @@
 <template>
-        <div class="msg">{{ state.message }}</div>
-        <div class="itxst">
-            <div class="group">
+    <div class="demo-collapse">
+    <el-collapse v-model="activeNames" @change="handleChange">
+      <el-collapse-item title="计算机科学与技术" class="l1" name="1">
+        <el-collapse-item title="  通识类课程" class="l2" name="2">
+          <el-collapse-item title="通识实践" class="l3" name="3">
+            <el-collapse-item title="通识实践必修" class="l4" name="4">
+                <div class="group">
+                <draggable
+                    :drag-class="dragClass"
+                    :list="state.modules.arr2"
+                    ghost-class="ghost"
+                    handle=".move"
+                    filter=".forbid"
+                    :force-fallback="true"
+                    chosen-class="chosenClass"
+                    animation="300"
+                    @start="onStart"
+                    @end="onEnd"
+                    :group="state.groupB"
+                    :fallback-class="true"
+                    :fallback-on-body="true"
+                    :touch-start-threshold="50"
+                    :fallback-tolerance="50"
+                    :move="onMove"
+                >
+                    <template #item="{ element }">
+                        <div class="item move">
+                            <label class="move">{{ element.name }}</label>
+                            <span>这里是B组</span>
+                        </div>
+                    </template>
+                </draggable>
+            </div>
+            </el-collapse-item>
+            <el-collapse-item title="专业必修" class="l4" name="4">
+                <div class="group">
                 <draggable
                     :drag-class="dragClass"
                     :list="state.modules.arr1"
@@ -30,40 +63,45 @@
                     </template>
                 </draggable>
             </div>
-            <div class="group">
-                <draggable
-                    :drag-class="dragClass"
-                    :list="state.modules.arr2"
-                    ghost-class="ghost"
-                    handle=".move"
-                    filter=".forbid"
-                    :force-fallback="true"
-                    chosen-class="chosenClass"
-                    animation="300"
-                    @start="onStart"
-                    @end="onEnd"
-                    :group="state.groupB"
-                    :fallback-class="true"
-                    :fallback-on-body="true"
-                    :touch-start-threshold="50"
-                    :fallback-tolerance="50"
-                    :move="onMove"
-                >
-                    <template #item="{ element }">
-                        <div class="item move">
-                            <label class="move">{{ element.name }}</label>
-                            <span>这里是B组</span>
-                        </div>
-                    </template>
-                </draggable>
-            </div>
+                </el-collapse-item>
+          </el-collapse-item>
+        </el-collapse-item>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+
+        <div class="msg">{{ state.message }}</div>
+        <div class="itxst">
+            
+            
         </div>
     </template>
     <script setup>
     import { ref, reactive } from "vue";
     //导入draggable组件
     import draggable from "vuedraggable";
-    
+    const tableData = [
+  {
+    date: '高数',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
     const state = reactive({
         message: "A组只能往B组拖到一个元素",
         groupA: {
@@ -173,4 +211,16 @@
     .fallbackClass {
         background-color: aquamarine;
     }
+    .l1 {
+  margin-left: 10px;
+}
+.l2 {
+  margin-left: 20px;
+}
+.l3 {
+  margin-left: 30px;
+}
+.l4 {
+  margin-left: 40px;
+}
     </style>
